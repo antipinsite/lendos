3c6e7ae29a44ba7eabd7b52ec8800a90e5a5db13
tyapkin.rs <tyapkin.roman@skpari.ru>
2026-04-01 21:08:19 +0300

    init 4: инит из ада

---
+++ backend/src/api/afisha/routes/afisha.ts
@@ -0,0 +1,7 @@
+/**
+ * afisha router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::afisha.afisha');
---
+++ backend/src/api/afisha/services/afisha.ts
@@ -0,0 +1,7 @@
+/**
+ * afisha service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::afisha.afisha');
